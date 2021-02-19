54c10479595fad298ec7144fe6ac53470aab425b
Jisu <kjl0349@naver.com>
2021-02-19 20:36:12 +0900

    <Counter> 컴포넌트 : + 함수, - 함수 생성 및 연결

--- react_counter/src/component/Counter.js
+++ react_counter/src/component/Counter.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Counter() {
+    const [num, setNum] = useState(0);
+    const onIncrease = () => {
+        setNum(num + 1);
+    }
+    const onDecrease = () => {
+        setNum(num - 1);
+    }
     return(
         <>
-            <h1>0</h1>
-            <button>+1</button>
-            <button>-1</button>
+            <h1>{num}</h1>
+            <button onClick={onIncrease}>+1</button>
+            <button onClick={onDecrease}>-1</button>
         </>
     );
 }
