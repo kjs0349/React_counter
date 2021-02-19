2c7fab4bb13ba19b0f11509a931d546f44de5e4c
Jisu <kjl0349@naver.com>
2021-02-19 20:27:23 +0900

    <Counter> 컴포넌트 생성 : 레이아웃 구조

---
+++ react_counter/src/component/Counter.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Counter() {
+    return(
+        <>
+            <h1>0</h1>
+            <button>+1</button>
+            <button>-1</button>
+        </>
+    );
+}
+
+export default Counter;
